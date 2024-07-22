f0de06f9823cdd963af857afbc15ceb186d35b46
arkajyoti-roy <arkajyotiroy445@gmail.com>
2024-07-22 13:45:19 +0530

    asd

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import "./style.css";
 import arka2 from "./images/arka2.png";
 const Footer = () => {
   return (
-    <div>
+    <div id="fott">
       <footer className="text-gray-600 body-font">
         <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
           <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,12 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-      "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+      // "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-  darkMode: "class",
-  plugins: [nextui()]
+  // darkMode: "class",
+  // plugins: [nextui()]
 }
