e6392c1f36a769fddf0c2022de20dfe2c0448188
arkajyoti-roy <arkajyotiroy445@gmail.com>
2024-07-24 10:48:11 +0530

    update 6 files and create 1 file

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,54 +1,19 @@
-/* eslint-disable react/no-unknown-property */
-// import React from "react";
 import arka2 from "./images/arka2.png";
 import { Link } from "react-scroll";
-import React from "react";
-import {
-  Navbar,
-  NavbarBrand,
-  NavbarContent,
-  NavbarItem,
-  NavbarMenuToggle,
-  NavbarMenu,
-  NavbarMenuItem,
-} from "@nextui-org/react";
-// import {AcmeLogo} from "./AcmeLogo.jsx";
-
-// import {Navbar, NavbarBrand, NavbarContent, NavbarItem,  DropdownItem, DropdownTrigger, Dropdown, DropdownMenu, Avatar} from "@nextui-org/react";
+import React, { useState } from "react";
 
 import "./style.css";
 const Nav = () => {
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-  // const menuItems = [
-  //   "Home",
-  //   "About",
-  //   "Skills",
-  //   "Services",
-  //   "Projects",
-  //   "Contact"
-
-  // ];
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
-    <Navbar className="shadow-md" onMenuOpenChange={setIsMenuOpen}>
-      <NavbarContent >
-
-        <NavbarBrand>
-          <Link
-            to="homee"
-            spy={true}
-            smooth={true}
-            offset={-100}
-            duration={500}
-            className="flex mb-4 md:mb-0 arka22"
-          >
-            <div className="flex flex-row float-right">
-              <img src={arka2} style={{ display: "none" }} alt="" />
-              <img src={arka2} alt="arka" />
-            </div>
-          </Link>
-
+    <div>
+      <header id="nav" className="text-gray-600 body-font">
+        <div className="w-full bg-white z-10 fixed flex items-end p-5 md:flex-row  shadow-md ">
           <Link
             className="cursor-pointer flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
             to="homee"
@@ -57,19 +22,139 @@ const Nav = () => {
             offset={-100}
             duration={500}
           >
-            <span className="ark  text-3xl">Arkajyoti</span>
+            <span className="ml-20 text-3xl">Arkajyoti</span>
           </Link>
-        </NavbarBrand>
-
-       
-        <NavbarMenuToggle
-          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-          className="sm:hidden "
-        />
-      </NavbarContent>
-
-      <NavbarContent className="nv hidden sm:flex gap-4 right-80">
-        <NavbarItem>
+          <nav className="md:ml-auto flex flex-wrap items-center mr-10 pb-1 text-base justify-center">
+            <Link
+              to="homee"
+              spy={true}
+              smooth={true}
+              offset={-100}
+              duration={500}
+              className="mr-5 hover:text-gray-900 font-semibold cursor-pointer"
+            >
+              Home
+            </Link>
+            <Link
+              to="about"
+              spy={true}
+              smooth={true}
+              offset={-100}
+              duration={500}
+              className="mr-5 hover:text-gray-900 font-semibold cursor-pointer"
+            >
+              About
+            </Link>
+            <Link
+              to="skills"
+              spy={true}
+              smooth={true}
+              offset={-100}
+              duration={500}
+              className="mr-5 hover:text-gray-900 font-semibold cursor-pointer"
+            >
+              Skills
+            </Link>
+            <Link
+              to="services"
+              spy={true}
+              smooth={true}
+              offset={-100}
+              duration={500}
+              className="mr-5 hover:text-gray-900 font-semibold cursor-pointer"
+            >
+              Services
+            </Link>
+            <Link
+              to="projects"
+              spy={true}
+              smooth={true}
+              offset={-100}
+              duration={500}
+              className="mr-5 hover:text-gray-900 font-semibold cursor-pointer"
+            >
+              Projects
+            </Link>
+            <Link
+              to="contact"
+              spy={true}
+              smooth={true}
+              offset={-100}
+              duration={500}
+              className="mr-5 hover:text-gray-900 font-semibold cursor-pointer"
+            >
+              Contact
+            </Link>
+          </nav>
+        </div>
+      </header>
+      <header id="nav2" className="text-gray-600 body-font">
+        <div className="w-full flex flex-row justify-between  shadow-md bg-white fixed pt-4 md:flex-row">
+         <div> <Link
+            to="homee"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={500}
+            className="flex title-font font-medium text-gray-900 mb-4 md:mb-0"
+          >
+            <img src={arka2} className="arka22" alt="arka" />
+          </Link>
+          </div>
+<div>
+          <button onClick={toggleNavbar}>
+        {isOpen ? (
+          <svg
+            height="2em"
+            viewBox="-0.5 0 25 25"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M3 21.32L21 3.32001"
+              stroke="#000000"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M3 3.32001L21 21.32"
+              stroke="#000000"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        ) : (
+          <svg
+            height="2em"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M5 12H20"
+              stroke="#000000"
+              stroke-width="2"
+              stroke-linecap="round"
+            />
+            <path
+              d="M5 17H20"
+              stroke="#000000"
+              stroke-width="2"
+              stroke-linecap="round"
+            />
+            <path
+              d="M5 7H20"
+              stroke="#000000"
+              stroke-width="2"
+              stroke-linecap="round"
+            />
+          </svg>
+        )}
+      </button>
+      <div className={`navbar ${isOpen ? "open" : "closed"}`}>
+        <ul className="flex flex-col gap-3 ml-2 mt-1 text-xl">
           <Link
             to="homee"
             spy={true}
@@ -80,8 +165,6 @@ const Nav = () => {
           >
             Home
           </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
           <Link
             to="about"
             spy={true}
@@ -92,8 +175,6 @@ const Nav = () => {
           >
             About
           </Link>
-        </NavbarItem>
-        <NavbarItem>
           <Link
             to="skills"
             spy={true}
@@ -104,8 +185,6 @@ const Nav = () => {
           >
             Skills
           </Link>
-        </NavbarItem>
-        <NavbarItem>
           <Link
             to="services"
             spy={true}
@@ -116,8 +195,6 @@ const Nav = () => {
           >
             Services
           </Link>
-        </NavbarItem>
-        <NavbarItem>
           <Link
             to="projects"
             spy={true}
@@ -128,8 +205,6 @@ const Nav = () => {
           >
             Projects
           </Link>
-        </NavbarItem>
-        <NavbarItem>
           <Link
             to="contact"
             spy={true}
@@ -140,92 +215,17 @@ const Nav = () => {
           >
             Contact
           </Link>
-        </NavbarItem>
-      </NavbarContent>
-      <NavbarContent justify="end"></NavbarContent>
-      <NavbarMenu>
-        {/* {menuItems.map((item, index) => (
-          <NavbarMenuItem key={`${item}-${index}`}>
-            <Link
-              color={
-                index === 2 ? "primary" : index === menuItems.length - 1 ? "danger" : "foreground"
-              }
-              className="w-full"
-              href="#"
-              size="lg"
-            >
-              {item}
-            </Link>
-          </NavbarMenuItem>
-        ))} */}
+        </ul>
+      </div>
+      </div>
 
-        <Link
-          to="homee"
-          spy={true}
-          smooth={true}
-          offset={-100}
-          duration={500}
-          className="mr-5 hover:text-gray-900 font-semibold cursor-pointer"
-        >
-          Home
-        </Link>
+        </div>
+        <br />
+        <br />
+      </header>
 
-        <Link
-          to="about"
-          spy={true}
-          smooth={true}
-          offset={-100}
-          duration={500}
-          className="mr-5 hover:text-gray-900 font-semibold cursor-pointer"
-        >
-          About
-        </Link>
-
-        <Link
-          to="skills"
-          spy={true}
-          smooth={true}
-          offset={-100}
-          duration={500}
-          className="mr-5 hover:text-gray-900 font-semibold cursor-pointer"
-        >
-          Skills
-        </Link>
-
-        <Link
-          to="services"
-          spy={true}
-          smooth={true}
-          offset={-100}
-          duration={500}
-          className="mr-5 hover:text-gray-900 font-semibold cursor-pointer"
-        >
-          Services
-        </Link>
-
-        <Link
-          to="projects"
-          spy={true}
-          smooth={true}
-          offset={-100}
-          duration={500}
-          className="mr-5 hover:text-gray-900 font-semibold cursor-pointer"
-        >
-          Projects
-        </Link>
-
-        <Link
-          to="contact"
-          spy={true}
-          smooth={true}
-          offset={-100}
-          duration={500}
-          className="mr-5 hover:text-gray-900 font-semibold cursor-pointer"
-        >
-          Contact
-        </Link>
-      </NavbarMenu>
-    </Navbar>
+      
+    </div>
   );
 };
 
