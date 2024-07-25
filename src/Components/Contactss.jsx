244be6ce8cfd1ab44e1ffcc0e2613b723d7dd0f9
arkajyoti-roy <arkajyotiroy445@gmail.com>
2024-07-25 22:53:23 +0530

    update Contactss.jsx

--- src/Components/Contactss.jsx
+++ src/Components/Contactss.jsx
@@ -10,7 +10,7 @@ const Contactss = () => {
     e.preventDefault();
 
     emailjs
-      .sendForm("service_wh959z8", "template_1i4023f", form.current, {
+      .sendForm("service_xtnuyje", "template_1i4023f", form.current, {
         publicKey: "Ymy3TIK4Q12f8c5_g",
       })
       .then(
